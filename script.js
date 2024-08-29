3ea9c13b093cec4f195fd5a27fb75bc992e9c2e2
ALNoman78 <abdullaalno078@gmail.com>
2024-08-29 13:56:23 +0600

    javaScript work start

---
+++ script.js
@@ -0,0 +1,68 @@
+// console.log("Abdullah Al Noman");
+// // let numOne = 20.3;
+// // let number = 0.2;
+// // let total = numOne + number;
+// // console.log(total);
+
+// let str = parseInt("20.12");
+// console.log(str);
+// // direct covert number
+
+// // floating number 
+
+// let num3  = parseFloat("20.12");
+// console.log(num3);
+
+// let numberOne = 0.1;
+// let numberTwo = 0.2;
+// let sumTotal = numberOne + numberTwo;
+// console.log(summation.toFixed(4));
+
+// let num = 10;
+// let sum = Number(num)
+// console.log(sum);
+
+// let str = 10;
+// let str1 = "10"
+// let total = str - str1;
+// console.log(total);
+
+// // 
+// let add = 10;
+// let b = "10";
+// let x = add + b
+// console.log(x);
+
+
+
+let appel = 300;
+let orange = 400;
+let totalCost = appel + orange; // print 600 $
+let moneyHave = 1000;    // total amount
+let extraMoney = moneyHave - totalCost; // total amount - totalCost
+console.log(extraMoney) // extra money 400
+
+
+//  average mark
+
+let math = 75.25
+let physics = 65
+let chemistry = 80
+let higherMath = 35.45
+let english = 99.50
+let totalMark = math +  physics + chemistry + higherMath + english
+// console.log(totalMark);
+let avg = totalMark
+console.log(avg.toFixed(2))
+
+// remainder
+let x = 119;
+x %= 5;
+console.log(x);
+
+// variable declaration
+let firstName = "Abdullah AL Noman"
+let lastName = "Mondol";
+let fullName = firstName + lastName;
+let age =21;
+console.log("This is my full name ", fullName + " I'm ", + age)
